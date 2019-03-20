188a724d20d1403c9852a8c133bf02d6c90aa25c
Martial AGBANGLA <amour.martial@gmail.com>
2019-03-20 02:18:28 -0400

    mon deuxième commit

---
+++ mti777-spiders/mti777/models.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import Column, String, Integer
+
+# db settings
+dbuser = 'magbangla' #DB username
+dbpass = 'password' #DB password
+dbhost = 'db4free.net' #DB host
+dbname = 'mti777' #DB database name
+engine = create_engine("mysql://%s:%s@%s/%s?charset=utf8&use_unicode=0"
+                       %(dbuser, dbpass, dbhost, dbname),
+                       echo=False,
+                       pool_recycle=1800)
+db = scoped_session(sessionmaker(autocommit=False,
+                                 autoflush=False,
+                                 bind=engine))
+
+Base = declarative_base()
+
+class annonces(Base):
+    __tablename__ = 'annonces'
+
+    id = Column(Integer, primary_key=True)
+    id_annonces=Column(String(20))
+    adresse=Column(String(100))
+    prix_mini=Column(Integer)
+    prix_maxi=Column(Integer)
+    details=Column(String(1000))
+    url_annonce=Column(String(500))
+
+    def __init__(self, id=None, id_annonces=None, adresse=None, prix_mini=None,prix_maxi=None,details=None,url_annonce=None):
+
+        self.id = id
+        self.id_annonces=id_annonces
+        self.adresse=adresse
+        self.prix_mini=prix_mini
+        self.prix_maxi=prix_maxi
+        self.details=details
+        self.url_annonce=url_annonce
+    def __repr__(self):
+        return "<AllData: id='%d', title='%s', url='%s', desc='%s'>" % (self.id, self.title, self.url, self.desc)
--- mti777-spiders/mti777/pipelines.py
+++ mti777-spiders/mti777/pipelines.py
@@ -4,26 +4,15 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import Column, String, Integer
-
-# db settings
-dbuser = 'root' #DB username
-dbpass = 'password' #DB password
-dbhost = 'localhost' #DB host
-dbname = 'scrapyspiders' #DB database name
-engine = create_engine("mysql://%s:%s@%s/%s?charset=utf8&use_unicode=0"
-                       %(dbuser, dbpass, dbhost, dbname),
-                       echo=False,
-                       pool_recycle=1800)
-db = scoped_session(sessionmaker(autocommit=False,
-                                 autoflush=False,
-                                 bind=engine))
-
-Base = declarative_base()
+from models import AllData, db
 
 class Mti777Pipeline(object):
+
     def process_item(self, item, spider):
+        # create a new SQL Alchemy object and add to the db session
+        record = AllData(title=item['title'][0].decode('unicode_escape'),
+                         url=item['url'][0],
+                         desc=desc)
+        db.add(record)
+        db.commit()
         return item
